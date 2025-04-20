8bb577265f4c595af207da9d3b6ce2da0477eb49
HenzoThomaz <henzothomaz9@gmail.com>
2025-04-19 22:21:09 -0300

    .

--- cadastro.py
+++ cadastro.py
@@ -1,20 +1,27 @@
 from flask import Flask, render_template, request, url_for,redirect,Blueprint
+import mysql.connector
 
 #aqui estoy trasnformando o arquivo em um blueprint, que serve para conectar ao arquivo princpal e meio que mandar suas fuções, o app.py sendo o principal, isso serve para que possamos ter varios codicos python em vez de um só no back-end, dexando mais organizado
 cadastro_bp = Blueprint('cadastro',__name__, url_prefix='/cadastro')
+
+
 
 app = Flask(__name__)
 
 #aqui nos vamos colocar a importação do banco de dados 
 def conectar_bd():
-#temo que aprender a conectar o bd e colocar aqui então por enquanto so vai ter um pass
-    pass
+    banco = mysql.connector.connect(
+  host="localhost",
+  user="seu_usuario",
+  password="sua_senha",
+  database="seu_banco_de_dados"
+)
 
-#falta criar a função propriamnete dita
 @cadastro_bp.route('/')
 def pagina_cadastro():
     return render_template('cadastro.html')
 
+#falta criar a função propriamnete dita
 @cadastro_bp.route('/criar_conta', methods=['POST'])
 def criar_conta():
     pass
