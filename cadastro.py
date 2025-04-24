15b9ba151fc8751c86e0b65b93b0ced7ab2d5ff0
Nathan Francisco <wdkoty@gmail.com>
2025-04-24 14:17:25 -0300

    fazendo o css do criar conta

--- cadastro.py
+++ cadastro.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for,redirect,Blueprint
-import _mysql_connector
+import mysql.connector
 
 #aqui estoy trasnformando o arquivo em um blueprint, que serve para conectar ao arquivo princpal e meio que mandar suas fuções, o app.py sendo o principal, isso serve para que possamos ter varios codicos python em vez de um só no back-end, dexando mais organizado
 cadastro_bp = Blueprint('cadastro',__name__, url_prefix='/cadastro')
