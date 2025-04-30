a971da4f5e2478aa1bd79c5553e2cfb68e1785a0
HenzoThomaz <henzothomaz9@gmail.com>
2025-04-29 23:44:48 -0300

    eh pra estar funcionando agora

--- login.py
+++ login.py
@@ -13,25 +13,24 @@ def pagina_login():
     return render_template('login.html')
 
 @login_bp.route('/login.html', methods=['POST'])
-def login(nome,senha):
+def login():
     if request.method == 'POST':
-        request.form.get['nome'] == nome 
-        request.form.get['senha'] == senha
+        nome = request.form.get('nome')
+        senha = request.form.get('senha')
 
-#(cursor = conexão_bd.cursor()
-        #query = "SELECT * FROM usuarios WHERE nome = %s AND senha = %s"
-        #cursor.execute(query, (nome, senha))
-        #usuario = cursor.fetchone()
-        #cursor.close()
+        cursor = conexão_bd.cursor()
+        query = "SELECT * FROM usuarios WHERE nome = %s AND senha = %s"
+        cursor.execute(query, (nome, senha))
+        usuario = cursor.fetchone()
+        cursor.close()
 
-    #if usuario:
-            # Se as credenciais forem válidas, redirecione para a página principal
-            #return render_template('principal.html')
-        #else:
-            # Se as credenciais forem inválidas, exiba a mensagem de erro novamente no formulário de login
-            #return render_template('login.html', mensagem="Nome de usuário ou senha incorretos")
-    #else:
-        # Se o método da requisição não for POST (por exemplo, GET), apenas renderize a página de login
-        #return render_template('login.html', mensagem="Insira as informações para continuar.")
-
+        if usuario:
+            # Se tudo estiver certinho no bd, redirecione para a página principal
+            return render_template('principal.html')
+        else:
+            # Se n exiba a mensagem de erro novamente no formulário de login do html
+            return render_template('login.html', mensagem="Nome de usuário ou senha incorretos")
+    else:
+        # Se o método da requisição não for POST, apenas renderize a página de login
+        return render_template('login.html', mensagem="Insira as informações para continuar.")
 
