4195e499d42df8cb261223715716310c73deee45
HenzoThomaz <henzothomaz9@gmail.com>
2025-05-21 16:36:25 -0300

    registro aparentemente funcionado

--- registros.py
+++ registros.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for, redirect
 import mysql.connector
 
-registros_bp = Blueprint('registros', __name__, url_prefix='/registros')
+registros_bp = Blueprint('registros', __name__,)
 
 def conectar_bd():
     return mysql.connector.connect(
@@ -17,32 +17,19 @@ def registrar():
     cursor = conn.cursor(dictionary=True)
 
     if request.method == 'POST':
-        id_animal = request.form['animal']
-        id_vacina = request.form['vacina']
+        animais = request.form['animal']
+        vacina = request.form['vacina']
         data_aplicacao = request.form['data_aplicacao']
         observacoes = request.form['observacoes']
         cursor.execute("""
-            INSERT INTO registros_vacina (id_animal, id_vacina, data_aplicacao, observacoes)
+            INSERT INTO registros_vacina (animais, vacina, data_aplicacao, observacoes)
             VALUES (%s, %s, %s, %s)
-        """, (id_animal, id_vacina, data_aplicacao, observacoes))
+        """, (animais, vacina, data_aplicacao, observacoes))
         conn.commit()
 
-    cursor.execute("SELECT * FROM animais")
-    animais = cursor.fetchall()
-
-    cursor.execute("SELECT * FROM vacinas")
-    vacinas = cursor.fetchall()
-
-    cursor.execute("""
-        SELECT a.nome AS animal, v.nome AS vacina, r.data_aplicacao, r.observacoes
-        FROM registros_vacina r
-        JOIN animais a ON r.id_animal = a.id
-        JOIN vacinas v ON r.id_vacina = v.id
-        ORDER BY r.data_aplicacao DESC
-    """)
-    registros = cursor.fetchall()
+    #registros = cursor.fetchall()
 
     cursor.close()
     conn.close()
 
-    return render_template('registros.html', animais=animais, vacinas=vacinas, registros=registros)
+    return redirect(url_for('registros.registrar')) and render_template('registro.html', mensagem ="Registro feito com sucesso!")
